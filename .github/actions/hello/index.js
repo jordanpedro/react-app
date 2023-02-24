a988d31df2c4995cd8e26f555cd4fb5f2866b097
jordan ricardo pedro <jordan.pedro@viavarejo.com.br>
2023-02-24 10:00:00 -0300

    pass

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -3,7 +3,6 @@ const github = require('@actions/github');
 const name = core.getInput('who-to-greet')
 console.log(`Hellow ${name}`)
 try{
-throw(new Error("MEnsagem de erro"));
 const time = new Date();
 core.setOutput("time", time.toTimeString());
 }
