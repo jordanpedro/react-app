0fbae7611d2b1d6b3e734b271a65af018cfcd62c
jordan ricardo pedro <jordan.pedro@viavarejo.com.br>
2023-02-27 08:01:00 -0300

    s

--- .github/actions/hello/index.js
+++ .github/actions/hello/index.js
@@ -1,7 +1,10 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
+const { ExitCode } = require('./dist');
 const name = core.getInput('who-to-greet')
 console.log(`Hellow ${name}`)
+core.debug("Salve comunidade DEBUG!")
+core.warning("Salve comunidade warning!")
 try{ 
 const time = new Date();
 core.setOutput("time", time.toTimeString());
