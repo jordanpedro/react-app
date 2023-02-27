d450090b4acbc8e8c46ff3fca756e1055a4d8dc4
jordan ricardo pedro <jordan.pedro@viavarejo.com.br>
2023-02-27 10:51:45 -0300

    vr

--- .github/actions/issues/index.js
+++ .github/actions/issues/index.js
@@ -1,16 +1,17 @@
 const core = require('@actions/core');
 const github = require('@actions/github');
- 
+const octokit = new Octokit();
+
 async function run(){
  
 try{ 
-    const token = core.getInput('token');
-    const title = core.getInput('title');
-    const body = core.getInput('body');
-    const assignees = core.getInput('assignees');
-    const octokit = new github.GitHub(token);
-
-    const response =  await octokit.issues.create({
+    const token = core.getInput('token')
+    const title = core.getInput('title')
+    const body = core.getInput('body')
+    const assignees = core.getInput('assignees')
+    const octokit = new github.getOctokit(GITHUB_TOKEN);
+    
+    const response =  await octokit.rest.issues.create({
         ...github.context.repo,        
         title,
         body,
